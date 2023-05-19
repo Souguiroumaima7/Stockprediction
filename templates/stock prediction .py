bccfaffb8a5b9740156ec88591ea2eefb27c4627
Oumaima Souguir <81293137+Souguiroumaima7@users.noreply.github.com>
2023-05-19 09:22:43 +0100

    first commit

---
+++ templates/stock prediction .py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install -q yfinance')
+
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+plt.style.use("fivethirtyeight")
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# For reading stock data from yahoo
+
+import yfinance as yf
+
+
+yf.pdr_override()
+
+# For time stamps
+from datetime import datetime
+
+
+# The tech stocks we'll use for this analysis
+tech_list = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+
+# Set up End and Start times for data grab
+tech_list = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+
+end = datetime.now()
+start = datetime(end.year - 1, end.month, end.day)
+
+for stock in tech_list:
+    globals()[stock] = yf.download(stock, start, end)
+    
+
+company_list = [AAPL, GOOG, MSFT, AMZN]
+company_name = ["APPLE", "GOOGLE", "MICROSOFT", "AMAZON"]
+
+for company, com_name in zip(company_list, company_name):
+    company["company_name"] = com_name
+    
+df = pd.concat(company_list, axis=0)
+df.tail(10)
+
+
+# In[4]:
+
+
+# Summary Stats
+AAPL.describe()
+
+
+# In[5]:
+
+
+# General info
+AAPL.info()
+
+
+# In[6]:
+
+
+# Let's see a historical view of the closing price
+plt.figure(figsize=(15, 10))
+plt.subplots_adjust(top=1.25, bottom=1.2)
+
+for i, company in enumerate(company_list, 1):
+    plt.subplot(2, 2, i)
+    company['Adj Close'].plot()
+    plt.ylabel('Adj Close')
+    plt.xlabel(None)
+    plt.title(f"Closing Price of {tech_list[i - 1]}")
+    
+plt.tight_layout()
+
+
+# In[ ]:
+
+
+
+
